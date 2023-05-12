8d969bb81d832dc6cb74c04078a3f297ffa98bf8
fabiSal <fabilucas97@gmail.com>
2023-05-12 19:18:03 +0200

    ejercicio2

--- ExamenProgramacion3Evaluacion/src/examenprogramacion3evaluacion/Ejercicio2.java
+++ ExamenProgramacion3Evaluacion/src/examenprogramacion3evaluacion/Ejercicio2.java
@@ -5,17 +5,49 @@
  */
 package examenprogramacion3evaluacion;
 
-/**
- *
- * @author DAW
- */
+import java.io.BufferedReader;
+import java.io.*;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Ejercicio2 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+//ruta donde esta guardado el ARCHIVO TXT DESCARGADO DE LA PROFE
+//ruta "C:\\Users\\DAW\\Documents\\NetBeansProjects\\Aparcamientos.txt"
+        String filePath = "C:\\Users\\DAW\\Documents\\NetBeansProjects\\Aparcamientos.txt";
+        File file = new File(filePath);
+        //BUFFER 
+
+        int numMixtos = 0;
+        int numResidentes = 0;
+        int numDisuasorios = 0;
+
+        try ( FileReader fileReader = new FileReader(file);  BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] words = line.split(" ");
+                for (String word : words) {
+                    if (word.equals("MIXTO")) {
+                        numMixtos++;
+                    } else if (word.equals("RESIDENTE")) {
+                        numResidentes++;
+                    } else if (word.equals("DISUASORIO")) {
+                        numDisuasorios++;
+                    }
+                }
+                System.out.printf("MIXTOS: %d, RESIDENTES: %d, DISUASORIOS: %d\n", numMixtos, numResidentes, numDisuasorios);
+                numMixtos = 0;
+                numResidentes = 0;
+                numDisuasorios = 0;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
 }
